30e8f6c815be75ecb4ca0c3f4b88f112fcdcd788
NguyenTam <tamn1395@gmail.com>
2024-08-15 21:22:23 +0700

    first commit

---
+++ homework.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo '<a href = "https://www.tiktok.com/foryou">Tiktok</a>'
+    ?>
+</body>
+</html>
